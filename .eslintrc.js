6c23949749cb78bc19aef60fd0c23dc71097188e
garvitvirmani <garvitvirmani@gmail.com>
2022-08-26 15:09:32 +0530

    Deployed on Firebase

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,8 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
   },
-  extends: [
-    "plugin:react/recommended",
-    "airbnb",
-    "react-app",
-    "react-app/jest",
-  ],
+  extends: ["plugin:react/recommended", "airbnb", "react-app"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
