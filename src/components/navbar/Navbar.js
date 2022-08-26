5c2b837209a59a96c04285371b79ea18ea909a8c
garvitvirmani <garvitvirmani@gmail.com>
2022-08-26 18:56:36 +0530

    nav-bar-garbage-bin

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-  AppBar, Toolbar, Tab, Tabs,
-} from "@mui/material";
+import { AppBar, Toolbar, Tab, Tabs } from "@mui/material";
 
 import useContentful from "../../useContentful";
 
@@ -12,9 +10,8 @@ function Navbar() {
 
   useEffect(() => {
     getNavbar().then((response) => setFields(response));
-    console.log(fields);
   }, []);
-  console.log(fields);
+
   return (
     <AppBar sx={{ background: "#fffefe" }} position="sticky" elevation={0}>
       <Toolbar>
@@ -25,11 +22,6 @@ function Navbar() {
           indicatorColor="secondary"
           onChange={(e, value) => setSelected(value)}
         >
-          {/* <Tab label="f1"></Tab>
-          <Tab label="f1"></Tab>
-          <Tab label="f1"></Tab>
-          {console.log("gggggggpg")}
-          {console.log(fields)} */}
           {fields && fields.map(({ title }) => <Tab label={title} />)}
         </Tabs>
       </Toolbar>
